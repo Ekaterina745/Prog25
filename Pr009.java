46cc7b4bc5e62aaa0096f1841b009f54f302ac09
filyushina <filkaterina69@gmail.com>
2025-10-14 13:30:13 +0700

    добавили простые операции с массивами

--- Pr009.java
+++ Pr009.java
@@ -14,6 +14,7 @@ class MyClass2 {
 	}
 }
 
+
 class Pr009 {
 	public static void main(String args[]) {
 		MyClass t1 = new MyClass();
@@ -31,5 +32,79 @@ class Pr009 {
 
 		System.out.println("BMW X5 перевозит " + bmw.pass + " пассажиров на " + bmw.range() + " км");
 		System.out.println("Мерседес Cls перевозит " + mercedes.pass + " пассажиров на " + mercedes.range() + " км");
+
+ 		System.out.println();
+//Работа с массивами
+		int sample[];
+		sample = new int[10];
+
+		int sample2[] = new int[20];
+		int i;
+
+		for(i = 0; i <10; i++)
+		       sample[i] = i;
+
+		for(i = 0; i < 10; i++)
+			System.out.println("Элемент [" + i + "]: " + sample[i]);	
+//Определение максимального и минимального массива
+	sample[0]= 39;
+	sample[1]= -19;
+	sample[2]= 33;
+	sample[3]= 45;
+	sample[4]= -239;
+	sample[5]= -50;
+	sample[6]= 87;
+	sample[7]= 91;
+	sample[8]= 07;
+	sample[9]= -24;
+	int min, max;
+	min = max = sample2[0];
+	for(i =1; i <20; i++) {
+       		if(sample2[i] < min) min = sample2[i];
+		 if(sample2[i] > max) max = sample2[i];
+	}
+	 System.out.println("минимальмальное значение: " + min + "\nМаксимальное значение: " + max);
+
+	  System.out.println();
+         for(i =1; i <10; i++) 
+		  System.out.println("Элемент [" + i + "]: " + sample2[i]);
+	 
+	  System.out.println();
+	 int sample3[] = {10,51,-24,-66,88,12,35,11,15,2,-99};
+	   for(i =1; i <= 10; i++)
+		     System.out.println("Элемент [" + i + "]: " + sample3[i]);
+
+	     System.out.println();
+
+	     //Пузырьковая сортировка массива
+	     int a, b, t;
+	     int size;
+
+	     size = 10;
+
+		  System.out.println();				     
+	     for  (a=1; a< size; a++)
+		     for(b = size-1; b >= a; b--) {
+				if(sample3[b-1] > sample3[b]) {
+					t = sample3[b-1];
+					sample3[b-1] = sample3[b];
+					sample3[b] = t;
+				}
+		     }
+		for(i = 0; i < size; i++)
+			  System.out.print(" " + sample3[i]);
+		  System.out.println();
+
+		  System.out.println();
+		  //Двумерные массивы
+		  int table[][] = new int[4][3];
+
+		  for (t=0; t<4; t++) {
+			  for(i=0; i<3; i++) {
+				table[t][i] = (t*3) + i + 1;
+				  System.out.print("\t" + table[t][i] + " ");
+			  }
+			  System.out.println();
+		  }
 	}
 }
