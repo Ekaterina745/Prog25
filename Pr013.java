896166d9c370091252a97f4371845bdffd655d9a
filyushina <filkaterina69@gmail.com>
2025-11-13 12:25:42 +0700

    Статические блоки, быстрая сортировка, сравнение скорости быстрой и пузырьковой сортировки

--- Pr013.java
+++ Pr013.java
@@ -14,8 +14,24 @@ class StaticDemo {
 //Статические методы могут использовать только статические переменные 
 //Статические методы могут вызывать только ставтические методы
 }
+class StaticBlock {
+	static double rootOf2;
+	static double rootOf3;
+	int a;
+	static {
+		 System.out.println("Внутри статического блока");
+		 rootOf2 = Math.sqrt(2.0);
+		 rootOf3 = Math.sqrt(3.0);
+	}
+	StaticBlock(String msg) {
+		a = 100;
+		 System.out.println(msg + " Значение а: " + a);
+		 System.out.println("Значение rootOf2 в конструкторе: " + StaticBlock.rootOf2);
+	}
+}
 class Pr013 {
 	public static void main(String args[]) {
+		//Демонстрация статиеских переменныъх
 		StaticDemo ob1 = new StaticDemo();
 		StaticDemo ob2 = new StaticDemo();
 
@@ -35,7 +51,7 @@ class Pr013 {
 		System.out.println("ob1.sum(): " + ob1.sum());
 		System.out.println("ob2.sum(): " + ob2.sum());
 		System.out.println();
-
+		//Демонстрация статического метода 
 		System.out.println("Значение val: " + StaticDemo.val);
 		System.out.println("Значение, возвращаемое методом valDiv2: " + StaticDemo.valDiv2());
 
@@ -43,5 +59,11 @@ class Pr013 {
 		System.out.println("Значение val: " + StaticDemo.val);
 		System.out.println("Значение, возвращаемое методом valDiv2: " + StaticDemo.valDiv2());
 		         
+		//Демонстрация статического блока
+		System.out.println();
+		StaticBlock ob = new StaticBlock("Внутри конструкции");
+
+		System.out.println("Квадратный корень из 2: " + StaticBlock.rootOf2);
+		System.out.println("Квадратный корень из 3: " + StaticBlock.rootOf3);
 	}
 }
