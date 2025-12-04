9f3abd37f60f7c4854e960d0748ba8d13b5aec66
filyushina <filkaterina69@gmail.com>
2025-12-04 11:22:08 +0700

    использование ключевого слова final

---
+++ final.java
@@ -0,0 +1,54 @@
+A a = new A();
+B b = new B();
+a = b
+a.meth()
+class A {
+    void meth() {
+        System.out.println("Метод meth() класса А");
+    }
+    final void meth2() {
+	    System.out.println("Метод meth2 не допускает переопределения в подклассах");
+    }
+}
+b = new B()
+b = new B()
+a = b
+a.meth()
+class B extends A {
+	void meth() {
+		System.out.println("Метод meth() класа B");
+	}
+	//Переопределение  метода meth2() запрещено
+	//void meth2()
+	//System.out.println("Метод meth2() не допускает переопределения ");
+}
+b = new B()
+a = b
+a.meth()
+a.meth2()
+final class C {
+    int i=10;
+}
+class D extends A {
+    int j = 20;
+}
+class ErrorMsg {
+    final int OUTERR = 0;
+    final int INERR = 1;
+    final int DISKERR =2;
+    final int INDEXERR = 3;
+    String[] msgs = {
+        "Ошибка вывода",
+        "Ошибка ввода",
+        "Переполнение диска",
+        "Выход индекса за границы массива"
+    };
+    String getErrorMsg(int i) {
+        if(i >= 0 & i < msgs.length)
+        return msgs[i];
+        else
+        return "Неизвестная ошибка";
+    }
+}
+err.getErrorMsg(err.DISKERR)
+err.getErrorMsg(err.OUTERR)
