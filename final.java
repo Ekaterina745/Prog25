e00e67fcdd192e57d9008be1c4ce33d9a627b7e4
filyushina <filkaterina69@gmail.com>
2025-12-04 13:30:23 +0700

    Работа с пакетами bookpack и bookpackext

--- final.java
+++ final.java
@@ -1,7 +1,3 @@
-A a = new A();
-B b = new B();
-a = b
-a.meth()
 class A {
     void meth() {
         System.out.println("Метод meth() класса А");
@@ -10,10 +6,6 @@ class A {
 	    System.out.println("Метод meth2 не допускает переопределения в подклассах");
     }
 }
-b = new B()
-b = new B()
-a = b
-a.meth()
 class B extends A {
 	void meth() {
 		System.out.println("Метод meth() класа B");
